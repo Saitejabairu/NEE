7b467c586c32767447f8e686e07bc7d26e69d727
Saitejabairu <131595664+Saitejabairu@users.noreply.github.com>
2024-09-17 05:19:50 +0530

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,36 +1,35 @@
+import os
 import cocotb
 from cocotb.triggers import Timer, RisingEdge
 from cocotb.clock import Clock
 
+@cocotb.test()
+async def test_case(dut):
+    # Create a clock
+    clock = Clock(dut.CLK, 10, units="ns")
+    cocotb.start_soon(clock.start())
 
-async def reset_seq(dut):
+    # Reset sequence
     dut.RST_N.value = 1
     await Timer(1, "ns")
     dut.RST_N.value = 0
     await Timer(1, "ns")
     await RisingEdge(dut.CLK)
     dut.RST_N.value = 1
-    pass
 
-
-@cocotb.test()
-async def test_case(dut):
+    # Test sequence
     dut.EN_next.value = 0
     dut.EN_start.value = 0
-    cocotb.start_soon(Clock(dut.CLK, 10, units="ns").start())
-    cocotb.start_soon(reset_seq(dut))
-    values = range(5)
-    results = []
     await Timer(10, "ns")
     await RisingEdge(dut.CLK)
     dut.EN_start.value = 1
     await RisingEdge(dut.CLK)
     dut.EN_start.value = 0
+    values = range(5)
+    results = []
     for idx, v in enumerate(values):
         dut.EN_next.value = 1
         dut.next_k.value = v
         await RisingEdge(dut.CLK)
         results.append(dut.next.value.integer)
     cocotb.log.info(f"Output is {hex(sum(results))}")
-
-
